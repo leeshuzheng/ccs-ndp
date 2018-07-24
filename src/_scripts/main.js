4657bee5b091f39ff7d284062853f5c20c73cd6a
Hadi Lee <hadi.lee@adelphi.digital>
2018-07-24 10:36:53 +0800

    initial commit, init yeogurt, added ontime as npm

---
+++ src/_scripts/main.js
@@ -0,0 +1,13 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+'use strict';
+
+import $ from 'jquery';
+import ontime from 'ontime';
+
+$(() => {
+  console.log('ccs-ndp main.js loaded');
+
+
+});
