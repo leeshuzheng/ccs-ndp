0873a1487456db2deb32642279a5787c74a14bbd
Hadi Lee <hadi.lee@adelphi.digital>
2018-07-25 10:01:18 +0800

    added entire logic for the app. todo: aesthetics and maybe fireworks tweak

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -4,10 +4,58 @@
 'use strict';
 
 import $ from 'jquery';
+import './f';
 import ontime from 'ontime';
 
 $(() => {
-  console.log('ccs-ndp main.js loaded');
 
+  window.production = false;
+
+  // time on first load
+  var hour = (new Date()).getHours(),
+  daybg = $('.day'),
+  nightbg = $('.night'),
+  touchtopledge = $('.touchtopledge'),
+  countholder = $('.count'),
+  count = 1000;
+
+  getbackground(hour);
+  countholder.html(count.toLocaleString()); // toLocaleString for adding comma to number
+
+  ontime({
+    cycle: [ '00:00', '30:00' ] // runs every half hour to check time of day
+  }, function (ot) {
+
+    var hournow = (new Date()).getHours();
+
+    getbackground(hournow);
+
+    console.log('30 mins to next run');
+
+    ot.done();
+    return
+  });
+
+  touchtopledge.on('click touchstart', function() {
+    count++;
+    count = count.toLocaleString();
+
+    countholder.fadeOut(200, function() {
+      $(this).html(count).fadeIn(200);
+    });
+    // countholder.html(count.toLocaleString());
+  });
+
+
+  // helper functions
+  function getbackground(hourval) {
+    if (hourval >= 14) {
+      nightbg.show();
+      daybg.hide();
+    } else {
+      nightbg.hide();
+      daybg.show();
+    }
+  }
 
 });
